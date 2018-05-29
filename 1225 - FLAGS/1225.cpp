9b2e6e20517c883cc4569df8e34edd00fcc8d462
Anuraag <dbaanurag@gmail.com>
2018-05-30 00:28:05 +0530

    flags

--- 1225 - FLAGS/1225.cpp
+++ 1225 - FLAGS/1225.cpp
@@ -11,10 +11,18 @@ int main()
   // 1 represents color white and 3 represents color red;
   for(lll i = 2; i <= 45; i++)
   {
-    dp[i][1] = dp[i - 1][3] + dp[i - 2][1];
-    // Total number of seqence of length l which ends with red is
-    // th total number of sequence of length l - 2 which ends with blue;
-    dp[i][3] = dp[i - 1][1] + dp[i - 2][3];
+    dp[i][1] = dp[i - 1][3] + dp[i - 2][3];
+    // Total number of sequence of length l which ends with red will be;
+    // The total number of sequences with length l - 1 which ends with white sum;
+    // total number of sequence of length l - 2 which ends with white;
+    // valid sequence for length 1 - W , R
+    // valid sequence for length 2 - WR (if seq ends with R then l-1 ending with W), RW (if seq ends with W then l-1 ending with R)
+    // valid sequence for length 3
+    // WRW (if seq ends with W then l-1 ending with W)
+    // WBR (if seq ends with R then l-2 ending with W)
+    // RBW (if seq ends with R then l-2 ending with R)
+    // RWR (if seq ends with R then l-1 ending with W)
+    dp[i][3] = dp[i - 1][1] + dp[i - 2][1];
   }
   lll nn;
 
